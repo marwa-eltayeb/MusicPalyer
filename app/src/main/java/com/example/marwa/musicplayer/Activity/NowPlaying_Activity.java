01064d80cacc2af4104246b2854de7a336a11075
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-04-30 02:22:14 +0200

    Music Player App

--- app/src/main/java/com/example/marwa/musicplayer/Activity/NowPlaying_Activity.java
+++ app/src/main/java/com/example/marwa/musicplayer/Activity/NowPlaying_Activity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -15,8 +17,8 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.marwa.musicplayer.R;
 import com.example.marwa.musicplayer.Adapter.SongAdapter;
+import com.example.marwa.musicplayer.R;
 
 public class NowPlaying_Activity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
@@ -365,6 +367,28 @@ public class NowPlaying_Activity extends AppCompatActivity implements SeekBar.On
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/main.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Return Back to the MainActivity
+            case R.id.home:
+                Intent intent = new Intent(NowPlaying_Activity.this,MainActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
