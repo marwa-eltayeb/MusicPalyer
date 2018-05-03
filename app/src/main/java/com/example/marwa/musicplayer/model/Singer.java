4c9471ecec9de007c3aa9f4f579a123ac80d1f91
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-05-03 19:20:49 +0200

    Music Player App

---
+++ app/src/main/java/com/example/marwa/musicplayer/model/Singer.java
@@ -0,0 +1,53 @@
+package com.example.marwa.musicplayer.model;
+
+/**
+ * Created by Marwa on 4/3/2018.
+ */
+
+public class Singer {
+
+    /** Name of the singer */
+    private String nameOfSinger;
+
+    /** Photo of the singer */
+    private int imageResourceId = NO_IMAGE_PROVIDED;
+
+    /** Constant value that represents no image was provided for any singer */
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+
+
+    /**
+     * Create a new Singer object.
+     *
+     * @param nameOfSinger is the name of singer.
+     * @param imageResourceId is the photo of singer.
+     */
+    public Singer(String nameOfSinger, int imageResourceId) {
+        this.nameOfSinger = nameOfSinger;
+        this.imageResourceId = imageResourceId;
+    }
+
+
+    /**
+     * Get name of singer.
+     */
+    public String getNameOfSinger() {
+        return nameOfSinger;
+    }
+
+    /**
+     * Get the photo of singer.
+     */
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this song.
+     */
+    public boolean hasImage() {
+        return imageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+}
